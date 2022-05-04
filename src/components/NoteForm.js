bdb8998930f69b73c4a6fd2370229310ca6bc125
Jason <jdclaeys@outlook.com>
2022-05-03 22:34:50 -0500

    add create note functionality and note rendering

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NoteForm = () => {
+const NoteForm = ({ onCreateSubmit }) => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   return (
-    <form>
+    <form onSubmit={e => onCreateSubmit(e, title, body)}>
       <label htmlFor='title'>
         <strong>Title</strong>
       </label>
-      <input className='u-full-width' type="text" id="title" placeholder='Enter note title...' />
+      <input 
+        className='u-full-width' 
+        type="text" 
+        id="title" 
+        placeholder='Enter note title...'
+        onChange={e => setTitle(e.target.value)}
+        value={title}
+      />
       <label htmlFor='body'>
         <strong>Body</strong>
       </label>
-      <input className='u-full-width' type='text' id='body' placeholder='Enter note body...' />
+      <input 
+        className='u-full-width' 
+        type='text' 
+        id='body' 
+        placeholder='Enter note body...'
+        onChange={e => setBody(e.target.value)}
+        value={body}
+      />
       <button type="submit">Save Note</button>
     </form>
   );
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NoteForm from '../NoteForm';
 
 const HomePage = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    if (!localStorage.getItem('notes')) {
+      localStorage.setItem('notes', '[]');
+    } else {
+      setNotes(JSON.parse(localStorage.getItem('notes')));
+    }
+  }, []);
+
+  const onCreateSubmit = (e, title, body) => {
+    e.preventDefault();
+  
+    const storedNotes = JSON.parse(localStorage.getItem('notes'));
+  
+    const hasLength = storedNotes.length > 0 ? true : false;
+  
+    const note = {
+      id: hasLength ? storedNotes[storedNotes.length - 1].id + 1 : 1,
+      title,
+      body
+    };
+  
+    storedNotes.push(note);
+    localStorage.setItem('notes', JSON.stringify(storedNotes));
+
+    setNotes(storedNotes);
+  };
+
+  const renderedNotes = notes.map(note => {
+    return (
+      <div key={note.id}>
+        <h5>{note.title}</h5>
+        <p>{note.body}</p>
+        <hr />
+      </div>
+    );
+  });
+
    return (
      <>
        <h1>Create Note</h1>
-       <NoteForm />
-       <h3>My Notes</h3>
-       <p>No notes yet.</p>
+       <NoteForm onCreateSubmit={onCreateSubmit} />
+       <h3 style={{ marginBottom: '4rem' }}>My Notes</h3>
+       {notes.length > 0 ? renderedNotes : <p>No notes yet.</p>}
      </>
    );
 };
